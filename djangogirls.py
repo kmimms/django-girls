44408bf236c38ea347d1a0c5832707dfa6385ef8
karis <karismimms@gmail.com>
2018-02-03 13:54:10 +0100

    first commit of django girls wahoo

---
+++ djangogirls.py
@@ -0,0 +1,6 @@
+people is {"karis":22, "jolien":28,"daniel":37}
+if people["karis"]> people["jolien"]
+   print ("karis is older than jolien")
+else:
+    print ("karis is not older than jolien")
+    print ("jolien is maybe the same age or older")
